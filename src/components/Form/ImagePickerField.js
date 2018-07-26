ca9eeedf90a9e6dc634f649234483852e0b00f15
Onaracs <brendan.scarano@gmail.com>
2018-07-26 06:49:23 -0400

    built out image picker field

--- src/components/Form/ImagePickerField.js
+++ src/components/Form/ImagePickerField.js
@@ -5,8 +5,56 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const ImagePickerField = ({ field }) => console.log('field', field) || (
-  <div>{field.location.photos.map(imgRef => <img src={imgRef} />)}</div>
+const Wrapper = styled.div`
+    display: flex;
+    overflow: scroll;
+`;
+
+const ImgWrapper = styled.div`
+    max-width: 200px;
+    max-height: 200px;
+    margin: 0 1rem;
+
+    > img {
+        flex: 1 1 160px;
+        margin: 10px;
+        ${({ isSelected }) => isSelected && 'border: 3px solid #1890ff'};
+    }
+
+    &:focus {
+        outline: 0;
+    }
+`;
+
+const ImagePickerField = ({ field }) => (
+  <Wrapper>
+    {field.location.photos.map((imgRef, i) => (
+      <ImgWrapper
+        onClick={() => field.input.onChange(imgRef)}
+        onKeyDown={() => field.input.onChange(imgRef)}
+        role="button"
+        tabIndex={0}
+        isSelected={field.input.value === imgRef}
+        key={imgRef}
+      >
+        <img
+          src={imgRef}
+          alt={`${field.location.name}-${i}`}
+        />
+      </ImgWrapper>
+    ))}
+  </Wrapper>
 );
+
+ImagePickerField.propTypes = {
+  field: PropTypes.shape({
+    disabled: PropTypes.bool,
+    input: PropTypes.object,
+    label: PropTypes.string,
+    location: PropTypes.object,
+    meta: PropTypes.object,
+    type: PropTypes.string,
+  }).isRequired,
+};
 
 export default ImagePickerField;
