6de0256e0c48d53e9d372a4e55d6bcd11e732e6f
Onaracs <brendan.scarano@gmail.com>
2018-08-09 07:06:01 -0400

    added success and error modals for form submission

--- src/layouts/NewLocation/index.js
+++ src/layouts/NewLocation/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { compose, withHandlers, withState } from 'recompact';
-import { reduxForm } from 'redux-form';
+import { reduxForm, reset, clearFields } from 'redux-form';
 import { Query } from 'react-apollo';
+import { Modal } from 'antd';
 import { loadData } from '../../redux/location';
 import firebase from '../../firebase';
 import Presentation from './Presentation';
@@ -18,26 +19,63 @@ const enhance = compose(
     state => ({
       initialValues: state.location.data,
       formValues: state.form.newLocation && state.form.newLocation.values,
-    }), { loadData },
+    }), { loadData, reset, clearFields },
   ),
   withState('placeId', 'setPlaceId', ''),
   withState('placeIdToSearch', 'setPlaceIdToSearch', ''),
+  withState('isModalOpen', 'setModalOpen', false),
+  withHandlers({
+    // eslint-disable-next-line
+    fireSuccessModal: ({ formValues, reset, clearFields }) => () => {
+      Modal.success({
+        title: 'Success',
+        content: (
+          <div>
+            <p>{formValues.name} saved to database</p>
+            <p>Check it out on {' '}
+              <a
+                href={`https://the-lash-hoorah.firebaseapp.com/city/${formValues.city}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                The Last Hoorah: {formValues.city}
+              </a>
+            </p>
+          </div>
+        ),
+        onOk() {
+          reset('newLocation');
+        },
+      });
+    },
+    fireErrorModal: () => () => {
+      Modal.error({
+        title: 'Error',
+        content: 'Something went wrong',
+        onOk() { console.log('closing modal...'); },
+      });
+    },
+  }),
   withHandlers({
     setPlaceIdToSearch: ({ placeId, setPlaceIdToSearch }) => () => {
       setPlaceIdToSearch(placeId);
     },
-    save: ({ formValues }) => () => {
+    save: ({ formValues, fireSuccessModal, fireErrorModal }) => async () => {
       const dataCopy = { ...formValues };
       delete dataCopy.photos;
       const key = dataCopy.name.toLowerCase().replace(/ /g, '');
-      console.log('dataCopy', dataCopy);
 
-      db
-        .collection(formValues.city)
-        .doc('locations')
-        .collection('data')
-        .doc(key)
-        .set(dataCopy);
+      try {
+        await db
+          .collection(formValues.city)
+          .doc('locations')
+          .collection('data')
+          .doc(key)
+          .set(dataCopy);
+        fireSuccessModal();
+      } catch (err) {
+        fireErrorModal();
+      }
     },
   }),
   reduxForm({
@@ -53,6 +91,7 @@ const App = ({
   save,
   loadData,
   formValues,
+  isModalOpen,
 }) => (
   <Query query={query} variables={{ id: placeIdToSearch }}>
     {({ data, loading }) => {
@@ -69,6 +108,7 @@ const App = ({
           setPlaceIdToSearch={setPlaceIdToSearch}
           save={save}
           selectedFilters={(formValues && formValues.filters) || []}
+          isModalOpen={isModalOpen}
         />
       );
     }}
